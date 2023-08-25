4b642417096b7c94feaa0143aa72e2a317fa0922
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-25 14:43:31 +0100

    update

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -13,13 +13,14 @@ class LIFOCache(BaseCaching):
         super().__init__()
     
     def put(self, key, item):
-        """put method"""
+        """ Add an item in the cache """
         if key and item:
             self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            latest_key = sorted(self.cache_data)[-2]
-            self.cache_data.pop(latest_key)
-            print(latest_key)
+            self.cache_data.pop(self.last_item)
+            print('DISCARD:', self.last_item)
+        if key:
+            self.last_item = key
     
     def get(self, key):
         """get method """
