235290e234207cda038e69e6733b3b29c512dd58
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-25 11:17:09 +0100

    update

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 0-main """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    def init(self):
+        """ Initiliaze """
+        super().__init__()
+    
+    def put(self, key, item):
+        """put function"""
+        if key and item:
+            self.cache_data[key] = item
+    
+    def get(self, key):
+        """get function"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
