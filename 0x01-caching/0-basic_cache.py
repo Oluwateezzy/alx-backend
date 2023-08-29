233bda39bd28e3e171e839091a728a0008595dab
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-29 14:44:36 +0100

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,12 +14,12 @@ class BasicCache(BaseCaching):
     def init(self):
         """ Initiliaze """
         super().__init__()
-    
+
     def put(self, key, item):
         """put function"""
         if key and item:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """get function"""
         if key is None or key not in self.cache_data:
