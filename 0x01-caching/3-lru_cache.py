e16de0bf3892ff31b153485755073a5552c44753
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-30 14:53:33 +0100

    update

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """ Least Recently used """
+
+    def __init__(self):
+        """ Initialized """
+        super().__init__()
+        self.cache_data = OrderedDict()
+    
+    def put(self, key, item):
+        if key and item:
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            lru = self.cache_data.popitem(last=False)
+            print('DISCARD:', lru[0])
+    
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
