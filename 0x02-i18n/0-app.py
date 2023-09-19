d2a85fdf2ea1a5bb16a134538d419ee054cb9af9
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-19 23:07:25 +0100

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Basic flask app """
 from flask import Flask, render_template
+from flask_babel import Babel
 
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 @app.route('/')
@@ -11,6 +13,13 @@ def index():
     """ index page """
     return render_template('0-index.html')
 
+
+class Config:
+    """ conig class """
+    LANGUAGES = ['en', 'fr']
+
+
+app.config.from_object(Config)
 
 if __name__ == '__main__':
     app.run()
