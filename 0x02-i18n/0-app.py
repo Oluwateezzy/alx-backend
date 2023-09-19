2a0fa2fb8e2fc100200c846b9fd483dc8984033d
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-19 22:46:04 +0100

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,12 +2,15 @@
 """ Basic flask app """
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     """ index page """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
