a91f7bbd678e09adba3917666e01079ce6eb360e
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-19 23:54:28 +0100

    update

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Basic flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ conig class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    """ index page """
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ retrieve the locale """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+app.config.from_object(Config)
+
+if __name__ == '__main__':
+    app.run()
